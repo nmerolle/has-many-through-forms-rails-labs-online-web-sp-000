78680fd1b5223f5c209658c7e3122e51d3030b3d
nmerolle <nmerolle11@hotmail.com>
2021-05-28 19:33:56 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
+    if params[:comment][:user_id].blank?
+      params[:comment].delete(:user_id)
+    end
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
+
 
   private
 
